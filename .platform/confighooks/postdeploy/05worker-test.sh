b75ada6a3b98a02bfc70538ebd1183029f97eb83
Anatolii <tolikshekera@gmail.com>
2022-12-13 21:09:38 +0200

    fix

--- .platform/confighooks/postdeploy/05worker-test.sh
+++ .platform/confighooks/postdeploy/05worker-test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 sudo rm /etc/init.d/supervisord
+sudo rm /etc/supervisor
 sudo easy_install supervisor
 sudo mkdir /etc/supervisor/
 sudo mkdir /etc/supervisor/conf.d/ -p
@@ -8,7 +9,8 @@ sudo touch /etc/supervisor/conf.d/laravel-worker.conf
 sudo touch /etc/supervisord.conf
 sudo cp /var/app/current/supervisord.conf /etc/supervisord.conf
 sudo cp /var/app/current/laravel-worker.conf /etc/supervisor/conf.d/laravel-worker.conf
-sudo touch /var/log/supervisor/
+sudo rm /var/log/supervisor
+sudo mkdir /var/log/supervisor/
 sudo touch /var/log/supervisor/supervisord.log
 sudo supervisord -c /etc/supervisord.conf
 sudo supervisorctl reread
